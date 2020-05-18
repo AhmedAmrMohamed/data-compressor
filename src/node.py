df4e4791b7c24c17dc41710ac1a31d9a1ec72b51
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-05-18 09:12:55 +0200

    init

---
+++ src/node.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(se,freq,char = None):
+        se.freq  = freq
+        se.char  = char if char else None
+        se.left  = None
+        se.right = None
+
+    def __str__(se):
+        return f'<{se.left}> {se.freq} : {se.char} <{se.right}>'
+    
+    def __repr__(se):
+        return se.__str__()
+
+    def isleaf(se):
+        return bool(se.char)
+
+    def __gt__(se,other):
+        return se.freq > other.freq
+    
+    def __lt__(se,other):
+        return se.freq <= other.freq
+    
+    def insert(se,other):
+        new = Node(se.freq+other.freq)
+        new.left  = se
+        new.right = other
+        return new
+
+
+
+def __test():
+    a = Node(2,'a')
+    b = Node(12,'b')
+    c = Node(10,'c')
+    
+    mx = max(a,b,c)
+    print(mx)
+    
+    a = a.insert(b)
+    z = a.insert(c)
+    print(z)
+    
+
