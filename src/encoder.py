32405fb2781f9742d070d88fc9a30f66451cfe40
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-11-10 10:09:42 +0200

    speedup by using int(str, 2)

--- src/encoder.py
+++ src/encoder.py
@@ -1,6 +1,6 @@
 from huffman import Huffman
 import pickle
-
+from time import time
 import logging
 logging.basicConfig(level='INFO')
 debug = logging.info
@@ -15,28 +15,13 @@ class Encoder:
         debug('encoding')
         msg    = self.msg
         key    = self.key
-        encodednum   = 1
-        debug(f'len(msg) {len(msg)}')
-        for let in msg:
-            huffcode = key[let]
-            for bit in huffcode:
-                encodednum<<=1
-                if bit == '1':
-                    encodednum|=1
-        return encodednum
+        encodedmsg   = '1'+''.join([ key[itr] for itr in msg])
+        print('intint')
+        return int(encodedmsg,2)
     
     def keytostr(self):
         key = self.key
         ret = [f'{itr}:{key[itr]}' for itr in key]
         return ','.join(ret)
-
-    def strtoint(self, strmsg):
-        number = 1
-        for i in strmsg:
-            number<<=1
-            if i == '1':
-                number|=1
-        return number
-
 
 
